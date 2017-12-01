95474403d75b5776bd3ae01d39884cf19e10278c
SenpaiPlz <senpai@plznotice.me>
2017-12-01 15:52:43 +0000

    Removed uneccisary files

--- Week_01/Day_4/destroy-instance.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-aws ec2 terminate-instances --instance-ids $1
-aws ec2 wait --region eu-west-1 instance-terminated --instance-ids $1
--- Week_01/Day_4/ec2-instance-check.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-while ! test -e '/ec2-init-done.markerfile'
-do
-	echo testing for markerfile
-    sleep 2
-done
