429059f2be5a37a2accb0e956cb0c079a9309e05
SenpaiPlz <senpai@plznotice.me>
2017-12-08 12:59:51 +0000

    removed week 2 from this repo

--- week2/client/src/common/framework/inject.js
+++
@@ -1,9 +0,0 @@
-module.exports=function(provided){
-    provided = provided || {};
-    return function(dependencyName, optional){
-        if(!provided[dependencyName] && !optional){
-            throw new Error("Required dependency <" + dependencyName + "> is not provided.")
-        }
-        return provided[dependencyName];
-    }
-};
